65195ac93ed93b221b1c117de5f6ec6cefd7d109
zhoulei <zhoulei_sr@qq.com>
2023-09-29 16:28:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from urllib.parse import urlparse
 import os
 import csv
 import concurrent.futures
+from collections import deque
 
 
 def logger_in():
@@ -43,17 +44,19 @@ logger = logger_in()
 class Stream:
 
     def __init__(self, stream_name, url_m3, save_path):
+        self.m3_EXTINF = []
+        self.ts_urls = []
         self.save_path = save_path
         self.ts_url = None
         self.targetDuration = None
         self.sequence = None
-        self.m3_tail = None  # 记录m3u8文件最后一行
+        self.m3_tail = None  # 记录临时处理的extinf值
         self.ts_tag = None  # 标记ts是否进行过更新，1=是；0=否
         self.stream_name = stream_name
         self.url_m3 = url_m3
         self.url_m3_p = urlparse(url_m3)
-        self.ts_ttfb = 0
-        self.ts_ttlb = 0
+        self.ts_tt_k = deque([])
+        self.ts_tt_v = deque([])
         self.m3_ttfb = 0
         self.m3_ttlb = 0
 
@@ -65,20 +68,26 @@ class Stream:
         :return:
         """
 
-        header_ = ['时间', '流名', 'sequence', 'm3_ttfb', 'm3_ttlb', 'ts_ttfb', 'ts_ttlb']
+        header_ = ['类型', 'sequence或ts_name', 'ttfb', 'ttlb']
 
-        self.process_m3u8()
-        if self.m3_ttfb == 0:
-            logger.error('"m_init" 初始化实例失败，"get_context" 结果为空; url: {}'.format(self.url_m3))
-            exit(1)
+        # 创建文件夹
+        stream_path = '{0}{1}/'.format(self.save_path, self.stream_name)
+        if not os.path.exists(stream_path):
+            os.makedirs(stream_path)
 
         # 创建记录时延数据的文件
-        if not os.path.exists('{0}{1}/1_{1}_delay_data.csv'.format(self.save_path, self.stream_name)):
+        csv_path = '{0}{1}/1_{1}_delay_data.csv'.format(self.save_path, self.stream_name)
+        if not os.path.exists(csv_path):
             # 编码使用
             with open('{0}{1}/1_{1}_delay_data.csv'.format(self.save_path, self.stream_name), 'a', newline='',
                       encoding='utf-8-sig') as f:
                 writer = csv.writer(f)
                 writer.writerows([header_])
+        # 获取一次m3u8文件，初始化实例
+        self.process_m3u8()
+        if self.m3_ttfb == 0:
+            logger.error('"m_init" 初始化实例失败，"get_context" 结果为空; url: {}'.format(self.url_m3))
+            exit(1)
 
     @staticmethod
     def get_context(url: str):
@@ -113,27 +122,23 @@ class Stream:
         return r_bytes, ttfb
 
     @staticmethod
-    def save_file(file_b, path, file_name):
+    def save_file(file_b, file_path):
         """
 
         :param file_b: 二进制文件
-        :param path: 要保存的绝对路径推荐 self.save_path+self.stream_name
-        :param file_name: 文件名，在文件夹下保存文件
+        :param file_path: 要保存的绝对路径推荐 self.save_path+self.stream_name
         :return:
         """
 
-        file = '{}{}'.format(path, file_name)
-        if not os.path.exists(path):
-            os.makedirs(path)
-        if not os.path.exists(file):
-            with open(file, 'wb') as f:
-                f.write(file_b)
+        with open(file_path, 'wb') as f:
+            f.write(file_b)
 
     def process_m3u8(self, ):
         """
         更新流实例属性，self.ts_url self.sequence self.targetDuration
         判断m3u8文件最后一行的格式，修改self.m3_tail属性，用于生成self.ts_url
         保存m3u8文件
+        v1.1: 修改为将所有ts uri保存到 self.m3_EXTINF，ts下载时遍历
         :return:
         """
         start_time = time.time()
@@ -142,71 +147,91 @@ class Stream:
 
         if r_b is None:
             logger.error('"process_m3u8" "if r_b is None" ，"get_context" 结果为空; url: {}'.format(self.url_m3))
-            self.m3_ttlb = 0
             return
 
         m3u8 = r_b.decode('utf-8')
         m3_l = m3u8.splitlines()
-        for line in m3_l:
-            if 'EXT-X-MEDIA-SEQUENCE' in line:
-                self.sequence = line.split(':')[1]
-            elif 'EXT-X-TARGETDURATION' in line:
-                self.targetDuration = line.split(':')[1]
 
-        if self.m3_tail != m3_l[-1]:
-            self.ts_tag = 1
+        while m3_l:
+            line = m3_l.pop(0)
+            if '#EXT-X-TARGETDURATION' in line:
+                self.targetDuration = line.split(':')[-1]
+                continue
+            elif '#EXT-X-MEDIA-SEQUENCE' in line:
+                self.sequence = line.split(':')[-1]
+                continue
+            elif '#EXTINF' in line:
+                self.m3_EXTINF.append(m3_l.pop(0))
 
-        else:
-            self.ts_tag = 0
-
-        self.m3_tail = m3_l[-1]
-        file_name_t = time.strftime('%Y_%m_%d_%H_%M_%S_', time.localtime())
-        self.save_file(r_b, '{}{}/'.format(self.save_path, self.stream_name), file_name_t + self.sequence + '.m3u8')
+        m3_name_t = time.strftime('%Y_%m_%d_%H_%M_%S_', time.localtime())
+        m3_path = '{}{}/{}{}.m3u8'.format(self.save_path, self.stream_name, m3_name_t, str(self.sequence))
+        self.save_file(r_b, m3_path)
         logger.info('{} m3_success：{}'.format(self.stream_name, self.url_m3))
 
-        if 'http' in self.m3_tail:
-            self.ts_url = self.m3_tail
-            return
-        elif '/' in self.m3_tail:
-            self.ts_url = '{}://{}{}'.format(self.url_m3_p.scheme, self.url_m3_p.netloc, self.m3_tail)
-            return
-        else:
-            uri_l = self.url_m3_p.path.split('/')
-            uri_l[-1] = self.m3_tail
-            uri = '/'.join(uri_l)
-            self.ts_url = '{}://{}{}'.format(self.url_m3_p.scheme, self.url_m3_p.netloc, uri)
+        while self.m3_EXTINF != []:
+            self.m3_tail = self.m3_EXTINF.pop()
+            if 'http' in self.m3_tail:
+                self.ts_urls.append(self.m3_tail)
+                return
+            elif '/' in self.m3_tail:
+                self.ts_urls.append('{}://{}{}'.format(self.url_m3_p.scheme, self.url_m3_p.netloc, self.m3_tail))
+                return
+            else:
+                uri_l = self.url_m3_p.path.split('/')
+                uri_l[-1] = self.m3_tail
+                uri = '/'.join(uri_l)
+                self.ts_urls.append('{}://{}{}'.format(self.url_m3_p.scheme, self.url_m3_p.netloc, uri))
 
     def process_ts(self):
         """
         读self.ts_url属性，下载保存ts文件，ttfb, ttlb 修改到属性，process_delay方法处理
         :return:
         """
-        start_time = time.time()
-        r_b, self.ts_ttfb = self.get_context(self.ts_url)
-        self.ts_ttlb = time.time() - start_time
 
-        if r_b is None:
-            logger.error('"process_m3u8" "if r_b is None" ，"get_context" 结果为空; url: {}'.format(self.url_m3))
-            self.ts_ttlb = 0
-            return
+        while self.ts_urls != []:
+            self.ts_url = self.ts_urls.pop()
+            ts_name = os.path.basename(urlparse(self.ts_url).path).split('/')[-1]
+            ts_path = '{}{}/{}'.format(self.save_path, self.stream_name, ts_name)
+            if not os.path.exists(ts_path):
+                self.ts_tt_k.append(ts_name)
 
-        ts_name = os.path.basename(urlparse(self.ts_url).path).split('/')[-1]
-        file_name_t = time.strftime('%Y_%m_%d_%H_%M_%S_', time.localtime())
-        self.save_file(r_b, '{}{}/'.format(self.save_path, self.stream_name), file_name_t + ts_name)
+                start_time = time.time()
+                r_b, ts_ttfb = self.get_context(self.ts_url)
+                ts_ttlb = time.time() - start_time
 
-        logger.info('{} ts_success：{}'.format(self.stream_name, self.ts_url))
+                if r_b is not None:
+                    self.save_file(r_b, ts_path)
+                    self.ts_tt_v.append([ts_ttfb, ts_ttlb])
+
+                    logger.info('{} ts_success：{}'.format(self.stream_name, self.ts_url))
+
+                else:
+                    logger.error('"process_m3u8" "if r_b is None" ，"get_context" 结果为空; url: {}'.format(self.url_m3))
+                    self.ts_tt_v.append([0, 0])
+                    return
+            # time.sleep(1)
 
     def process_delay(self):
         """
         每循环执行一次get_stream后调用该方法处理时延数据，追加写入csv
         :return:
         """
+        time_l = []
+
+        # 每次循环 m3u8 必有，所以直接执行
+        time_l.append(['m3u8', self.sequence, self.m3_ttfb, self.m3_ttlb])
+        # 判断有没有执行过ts，直到队列取完
+        while self.ts_tt_k:
+            time_l.append(['ts', self.ts_tt_k.popleft()] + self.ts_tt_v.popleft())
+
+        csv_path = '{0}{1}/1_{1}_delay_data.csv'.format(self.save_path, self.stream_name)
+
         time_ = time.strftime('%Y_%m_%d_%H_%M_%S', time.localtime())
-        with open('{0}{1}/1_{1}_delay_data.csv'.format(self.save_path, self.stream_name), 'a', newline='',
-                  encoding='utf-8-sig') as f:
-            writer = csv.writer(f)
-            writer.writerows(
-                [[time_, self.stream_name, self.sequence, self.m3_ttfb, self.m3_ttlb, self.ts_ttfb, self.ts_ttlb]])
+
+        if time_l:
+            with open(csv_path, 'a', newline='', encoding='utf-8-sig') as f:
+                writer = csv.writer(f)
+                writer.writerows(time_l)
 
     def get_stream(self):
         """
@@ -215,11 +240,7 @@ class Stream:
         """
         while True:
             self.process_m3u8()
-            if self.ts_tag == 1:
-                self.process_ts()
-            else:
-                self.ts_ttfb, self.ts_ttlb = 0, 0
-
+            self.process_ts()
             self.process_delay()
 
             # 间隔2/3个targetDuration时间获取一次
